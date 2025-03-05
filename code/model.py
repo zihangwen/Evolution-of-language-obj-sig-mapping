fd78b8c15dd60b4c9d8a8c0051ece6bceaf5a5a6
Zihang <wenzihang0506@gmail.com>
2025-03-04 18:41:39 -0600

    add einops

--- code/model.py
+++ code/model.py
@@ -2,6 +2,7 @@ import numpy as np
 from utilities import *
 from dataclasses import dataclass
 from typing import Type, List, Optional
+import einops
 
 
 @dataclass
@@ -34,6 +35,11 @@ class LanguageModel:
         for i in range(self.obj):
             sample_matrix[i, :] = np.random.multinomial(number_samples, self.P[i,:])
         return sample_matrix
+
+    @property
+    def self_comm_payoff(self):
+        assert self.P is not None and self.Q is not None
+        directional_comm(self, self)
 
 
 class LanguageModelSoftmax(LanguageModel):
@@ -91,10 +97,14 @@ class LanguageModelNormEPS(LanguageModel):
         self.Q = NormalizeEPS(self.Q)
 
 
+# def directional_comm(language1 : LanguageModel, language2 : LanguageModel) -> float:
+#     # return np.einsum('...ij,...ji->...', P, Q)
+#     return np.einsum('ij,ji', language1.P, language2.Q)
+
 def directional_comm(language1 : LanguageModel, language2 : LanguageModel) -> float:
     # return np.einsum('...ij,...ji->...', P, Q)
-    return np.einsum('ij,ji', language1.P, language2.Q)
-
+    # return np.einsum('ij,ji', language1.P, language2.Q)
+    return einops.einsum(language1.P, language2.Q, 'obj sig, sig obj ->')
 
 # def PairPayoff(language1 : LanguageModel, language2 : LanguageModel) -> float:
 #     payoff1 = np.einsum('...ij,...ji->...', language1.P, language2.Q)
@@ -107,3 +117,7 @@ def similar_language_check(language1: LanguageModel, language2: LanguageModel) -
         return True
     else:
         return False
+
+def stable_check(language: LanguageModel) -> bool:
+    # TODO: check whether the language is stabilized
+    pass
