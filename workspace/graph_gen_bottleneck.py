e3a530a128bdd0e8f192d78174146c9b011e8453
Zihang <wenzihang0506@gmail.com>
2025-06-10 12:51:09 -0500

    update deme-based models

--- workspace/graph_gen_bottleneck.py
+++ workspace/graph_gen_bottleneck.py
@@ -5,62 +5,67 @@ import os
 import copy
 import random
 from itertools import pairwise
+from pathlib import Path
 
 
 # Island: N, d, mig_rate
 # Bottleneck
-base_path = "/home/zihangw/EvoComm/"
+base_path = Path("/home/zihangw/EvoComm/")
 
 # %%
 # ----- ----- ----- ----- ----- bottleneck models ----- ----- ----- ----- ----- #
-n = 100
-num_dims = 10
+n = 1000
+num_demes_list = [5, 10, 20, 50, 100]
+# num_demes = 10
 # num_edges_list = [1, 2, 5, 10, 20, 50]
 # beta_list = [-3, -2, -1, 0, 1, 2]
-num_edges_list = [1, 5, 20]
+num_edges_list = [1, 5, 10, 20, 40]
 beta_list = [-3, 0, 2]
 
-output_path = os.path.join(base_path, "networks", "bottleneck_demes")
-os.makedirs(output_path,exist_ok=True)
+for num_demes in num_demes_list:
+    output_path = base_path / "networks" / f"bottleneck_demes_{num_demes}"
+    os.makedirs(output_path,exist_ok=True)
 
-dim_size = n // num_dims
-G_1 = nx.complete_graph(dim_size)
+    dim_size = n // num_demes
+    G_1 = nx.complete_graph(dim_size)
 
-G_base = nx.empty_graph()
-node_list = []
-G_list = []
-dim_index_list = []
-for i_dim in range(num_dims):
-    G_dim = copy.deepcopy(G_1)
-    mapping = {node: node + i_dim * dim_size for node in G_dim.nodes}
-    G_dim = nx.relabel_nodes(G_dim, mapping)
-    
-    G_base = nx.compose(G_base, G_dim)
-    node_list.append(G_dim.nodes)
-    G_list.append(G_dim)
-    dim_index_list.append(i_dim)
+    G_base = nx.empty_graph()
+    node_list = []
+    G_list = []
+    dim_index_list = []
+    for i_dim in range(num_demes):
+        G_dim = copy.deepcopy(G_1)
+        mapping = {node: node + i_dim * dim_size for node in G_dim.nodes}
+        G_dim = nx.relabel_nodes(G_dim, mapping)
+        
+        G_base = nx.compose(G_base, G_dim)
+        node_list.append(G_dim.nodes)
+        G_list.append(G_dim)
+        dim_index_list.append(i_dim)
 
-# %%
-pairs = list(pairwise(dim_index_list))
-for num_edges in num_edges_list:
-    for beta in beta_list:
-        count_m = 0
-        G = copy.deepcopy(G_base)
-        for i_edge in range(num_edges):
-            degree_list = [np.array(G_i.degree())[:,1].astype(float) for G_i in G_list]
-            prob_list = [degree ** beta for degree in degree_list]
-            prob_list = [prob / np.sum(prob) for prob in prob_list]
+    pairs = list(pairwise(dim_index_list))
+    for num_edges in num_edges_list:
+        for beta in beta_list:
+            count_m = 0
+            G = copy.deepcopy(G_base)
+            for i_edge in range(num_edges):
+                degree_list = [np.array(G_i.degree())[:,1].astype(float) for G_i in G_list]
+                prob_list = [degree ** beta for degree in degree_list]
+                prob_list = [prob / np.sum(prob) for prob in prob_list]
 
-            new_edge_list = []
-            for dim_1, dim_2 in pairs:
-                node_1 = np.random.choice(node_list[dim_1], 1, False, prob_list[dim_1])[0]
-                node_2 = np.random.choice(node_list[dim_2], 1, False, prob_list[dim_2])[0]
-                new_edge_list.append((node_1, node_2))
-            
-            # print(new_edge_list)
-            G.add_edges_from(new_edge_list)
-        print(nx.is_connected(G))
-        nx.write_edgelist(G, os.path.join(output_path,f"bn_{num_dims}_{num_edges}_m{str(beta)}_{str(count_m)}.txt"), data=False)
+                new_edge_list = []
+                for dim_1, dim_2 in pairs:
+                    node_1 = np.random.choice(node_list[dim_1], 1, False, prob_list[dim_1])[0]
+                    node_2 = np.random.choice(node_list[dim_2], 1, False, prob_list[dim_2])[0]
+                    new_edge_list.append((node_1, node_2))
+                
+                # print(new_edge_list)
+                G.add_edges_from(new_edge_list)
+            print(nx.is_connected(G))
+            nx.write_edgelist(G, os.path.join(output_path,f"bn_{num_demes}_{num_edges}_m{str(beta)}_{str(count_m)}.txt"), data=False)
             
 
 # os.listdir("/home/zihangw/EvoComm/networks/bottleneck")
+# %% test
+# G = nx.read_edgelist("/home/zihangw/EvoComm/networks/bottleneck_demes_100/bn_100_5_m0_0.txt", nodetype=int, data=False)
+# G.nodes
--- workspace/write_param.py
+++ workspace/write_param.py
@@ -1,141 +1,165 @@
+# %%
 import numpy as np
 import os
 import re
+from pathlib import Path
 
+BASE_PATH = Path("/home/zihangw/EvoComm/")
+# NET_PATH = BASE_PATH / "networks"
 
-if __name__ == "__main__":
-    # num_objects = 5
-    # num_sounds = 5
-    # num_languages = 100
-    # model_name_list = ["norm", "softmax"]
-    # graph_path_list = ["graphs/3-r.txt", "graphs/5-r.txt", "graphs/detour.txt", "graphs/star.txt", "graphs/wheel.txt", "graphs/wm_100.txt"]
-    # # graph_path_list = ["graphs/detour.txt", "graphs/wm_100.txt"]
-    # num_runs = int(1e5)
-    # out_path_base = "results"
-    # # n_trial = range(10)
+# %%
+# if __name__ == "__main__":
+# num_objects = 5
+# num_sounds = 5
+# num_languages = 100
+# model_name_list = ["norm", "softmax"]
+# graph_path_list = ["graphs/3-r.txt", "graphs/5-r.txt", "graphs/detour.txt", "graphs/star.txt", "graphs/wheel.txt", "graphs/wm_100.txt"]
+# # graph_path_list = ["graphs/detour.txt", "graphs/wm_100.txt"]
+# num_runs = int(1e5)
+# out_path_base = "results"
+# # n_trial = range(10)
 
-    # with open("param_agent.txt", "w") as f:
-    #     for model_name in model_name_list:
-    #         for graph_path in graph_path_list:
-    #             f.write("%d %d %d %s %s %d %s\n"%(num_objects, num_sounds, num_languages, model_name, graph_path, num_runs, out_path_base))
+# with open("param_agent.txt", "w") as f:
+#     for model_name in model_name_list:
+#         for graph_path in graph_path_list:
+#             f.write("%d %d %d %s %s %d %s\n"%(num_objects, num_sounds, num_languages, model_name, graph_path, num_runs, out_path_base))
 
-    # num_objects = 5
-    # num_sounds = 5
-    # num_languages = 10
-    # model_name_list = ["norm", "softmax"]
-    # num_runs = int(25_000)
+# num_objects = 5
+# num_sounds = 5
+# num_languages = 10
+# model_name_list = ["norm", "softmax"]
+# num_runs = int(25_000)
 
-    # graph_path_list = ["networks/toy_star/" + a for a in os.listdir("/home/zihangw/EvoComm/networks/toy_star")]
-    # out_path_base = "results/toy_star"
-    # # n_trial = range(10)
+# graph_path_list = ["networks/toy_star/" + a for a in os.listdir("/home/zihangw/EvoComm/networks/toy_star")]
+# out_path_base = "results/toy_star"
+# # n_trial = range(10)
 
-    # with open("param_toy_regular.txt", "a") as f:
-    #     for model_name in model_name_list:
-    #         for graph_path in graph_path_list:
-    #             f.write("%d %d %d %s %s %d %s\n"%(num_objects, num_sounds, num_languages, model_name, graph_path, num_runs, out_path_base))
-    
-    # num_languages = 100
-    # num_runs = int(50_000)
-    # graph_path_list = ["networks/regular_100_4_triangle/reg_tri_100_4_%d.txt" %i for i in range(0,100,5)]
-    # out_path_base = "results/regular_100_4_triangle"
-    # # n_trial = range(10)
+# with open("param_toy_regular.txt", "a") as f:
+#     for model_name in model_name_list:
+#         for graph_path in graph_path_list:
+#             f.write("%d %d %d %s %s %d %s\n"%(num_objects, num_sounds, num_languages, model_name, graph_path, num_runs, out_path_base))
 
-    # with open("param_toy_regular.txt", "a") as f:
-    #     for model_name in model_name_list:
-    #         for graph_path in graph_path_list:
-    #             f.write("%d %d %d %s %s %d %s\n"%(num_objects, num_sounds, num_languages, model_name, graph_path, num_runs, out_path_base))
+# num_languages = 100
+# num_runs = int(50_000)
+# graph_path_list = ["networks/regular_100_4_triangle/reg_tri_100_4_%d.txt" %i for i in range(0,100,5)]
+# out_path_base = "results/regular_100_4_triangle"
+# # n_trial = range(10)
 
-    # num_objects = 5
-    # num_sounds = 5
-    # model_name_list = ["norm", "softmax"]
-    # graph_folder_name = "bottleneck_demes"
-    # graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
-    # num_runs = int(100_000)
-    # out_path_base = f"results/{graph_folder_name}"
-    # # n_trial = range(10)
-    # sample_times = 100
-    # temperature = 1.0
-    # with open("../param_space/param_bottleneck_demes.txt", "w") as f:
-    #     for model_name in model_name_list:
-    #         for graph_path in graph_path_list:
-    #             f.write(f"{num_objects} {num_sounds} {model_name} {graph_path} {num_runs} {out_path_base} {sample_times} {temperature:.1f}\n")
+# with open("param_toy_regular.txt", "a") as f:
+#     for model_name in model_name_list:
+#         for graph_path in graph_path_list:
+#             f.write("%d %d %d %s %s %d %s\n"%(num_objects, num_sounds, num_languages, model_name, graph_path, num_runs, out_path_base))
 
-    # num_objects = 5
-    # num_sounds = 5
-    # model_name_list = ["norm", "softmax"]
-    # sample_times_list = [1, 5, 10, 20, 40, 100]
-    # temperature_list = [1.0, 2.0, 5.0, 10.0]
+# num_objects = 5
+# num_sounds = 5
+# model_name_list = ["norm", "softmax"]
+# graph_folder_name = "bottleneck_demes"
+# graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
+# num_runs = int(100_000)
+# out_path_base = f"results/{graph_folder_name}"
+# # n_trial = range(10)
+# sample_times = 100
+# temperature = 1.0
+# with open("../param_space/param_bottleneck_demes.txt", "w") as f:
+#     for model_name in model_name_list:
+#         for graph_path in graph_path_list:
+#             f.write(f"{num_objects} {num_sounds} {model_name} {graph_path} {num_runs} {out_path_base} {sample_times} {temperature:.1f}\n")
 
-    # graph_folder_name = "temp_sample_size"
-    # graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
-    # num_runs = int(50_000)
-    # out_path_base = f"results/{graph_folder_name}"
-    # # n_trial = range(10)
-    
-    # with open(f"../param_space/param_{graph_folder_name}.txt", "w") as f:
-    #     for model_name in model_name_list:
-    #         for graph_path in graph_path_list:
-    #             for sample_times in sample_times_list:
-    #                 for temperature in temperature_list:
-    #                     f.write(f"{num_objects} {num_sounds} {model_name} {graph_path} {num_runs} {out_path_base} {sample_times} {temperature}\n")
-    #                     # f.write("%d %d %s %s %d %s\n"%(num_objects, num_sounds, model_name, graph_path, num_runs, out_path_base))
-    
-    # ----- param_bottleneck_demes_invade ----- #
-    # num_objects = 5
-    # num_sounds = 5
-    # graph_folder_name = "bottleneck_demes"
-    # graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
+# num_objects = 5
+# num_sounds = 5
+# model_name_list = ["norm", "softmax"]
+# sample_times_list = [1, 5, 10, 20, 40, 100]
+# temperature_list = [1.0, 2.0, 5.0, 10.0]
 
-    # graph_folder_name = "graphs"
-    # graph_path_list += [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
-    
-    # num_trials = int(10_000)
-    # with open("../param_space/param_bottleneck_demes_invade.txt", "w") as f:
-    #         for graph_path in graph_path_list:
-    #             f.write(f"{num_objects} {num_sounds} {graph_path} {num_trials}\n")
-    
-    # ----- param_PA_invade ----- #
-    # num_objects = 5
-    # num_sounds = 5
-    # graph_folder_name = "PA_100"
-    # graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
+# graph_folder_name = "temp_sample_size"
+# graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
+# num_runs = int(50_000)
+# out_path_base = f"results/{graph_folder_name}"
+# # n_trial = range(10)
 
-    # graph_folder_name = "social_network"
-    # graph_path_list += [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
-    
-    # num_trials = int(10_000)
-    # with open("../param_space/param_PA_invade.txt", "w") as f:
-    #         for graph_path in graph_path_list:
-    #             f.write(f"{num_objects} {num_sounds} {graph_path} {num_trials}\n")
-    
-    # ----- param_regular_invade ----- #
-    # num_objects = 5
-    # num_sounds = 5
-    # graph_folder_name = "paper_regular_100"
-    # graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
-    # graph_path_list = sorted(graph_path_list, key=lambda s: tuple(map(int, re.findall(r"\d+", s))))
+# with open(f"../param_space/param_{graph_folder_name}.txt", "w") as f:
+#     for model_name in model_name_list:
+#         for graph_path in graph_path_list:
+#             for sample_times in sample_times_list:
+#                 for temperature in temperature_list:
+#                     f.write(f"{num_objects} {num_sounds} {model_name} {graph_path} {num_runs} {out_path_base} {sample_times} {temperature}\n")
+#                     # f.write("%d %d %s %s %d %s\n"%(num_objects, num_sounds, model_name, graph_path, num_runs, out_path_base))
 
-    # # graph_path_list = graph_path_list[::5]
+# ----- param_bottleneck_demes_invade ----- #
+# num_objects = 5
+# num_sounds = 5
+# graph_folder_name = "bottleneck_demes"
+# graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
 
-    # num_trials = int(10_000)
-    # with open("../param_space/param_regular_invade.txt", "w") as f:
-    #     for graph_path in graph_path_list:
-    #         f.write(f"{num_objects} {num_sounds} {graph_path} {num_trials}\n")
+# graph_folder_name = "graphs"
+# graph_path_list += [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
 
-    # ----- param_regular_4_invade ----- #
-    num_objects = 5
-    num_sounds = 5
-    graph_folder_name = "regular_100_4_triangle"
-    graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
-    graph_path_list = sorted(graph_path_list, key=lambda s: tuple(map(int, re.findall(r"\d+", s))))
+# num_trials = int(10_000)
+# with open("../param_space/param_bottleneck_demes_invade.txt", "w") as f:
+#         for graph_path in graph_path_list:
+#             f.write(f"{num_objects} {num_sounds} {graph_path} {num_trials}\n")
 
-    graph_path_list = graph_path_list[::5]
+# ----- param_PA_invade ----- #
+# num_objects = 5
+# num_sounds = 5
+# graph_folder_name = "PA_100"
+# graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
 
-    num_trials = int(10_000)
-    with open("../param_space/param_regular_4_invade.txt", "w") as f:
+# graph_folder_name = "social_network"
+# graph_path_list += [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
+
+# num_trials = int(10_000)
+# with open("../param_space/param_PA_invade.txt", "w") as f:
+#         for graph_path in graph_path_list:
+#             f.write(f"{num_objects} {num_sounds} {graph_path} {num_trials}\n")
+
+# ----- param_regular_invade ----- #
+# num_objects = 5
+# num_sounds = 5
+# graph_folder_name = "paper_regular_100"
+# graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
+# graph_path_list = sorted(graph_path_list, key=lambda s: tuple(map(int, re.findall(r"\d+", s))))
+
+# # graph_path_list = graph_path_list[::5]
+
+# num_trials = int(10_000)
+# with open("../param_space/param_regular_invade.txt", "w") as f:
+#     for graph_path in graph_path_list:
+#         f.write(f"{num_objects} {num_sounds} {graph_path} {num_trials}\n")
+
+# %%
+# ----- param_regular_4_invade ----- #
+# num_objects = 5
+# num_sounds = 5
+# graph_folder_name = "regular_100_4_triangle"
+# graph_path_list = [f"networks/{graph_folder_name}/" + a for a in os.listdir(f"/home/zihangw/EvoComm/networks/{graph_folder_name}")]
+# graph_path_list = sorted(graph_path_list, key=lambda s: tuple(map(int, re.findall(r"\d+", s))))
+
+# graph_path_list = graph_path_list[::5]
+
+# num_trials = int(10_000)
+# with open("../param_space/param_regular_4_invade.txt", "w") as f:
+#     for graph_path in graph_path_list:
+#         f.write(f"{num_objects} {num_sounds} {graph_path} {num_trials}\n")
+
+# %%
+# ----- deme model invasion ----- #
+num_objects = 5
+num_sounds = 5
+graph_folder_list = [f"bottleneck_demes_{num_demes}" for num_demes in [5, 10, 20, 50, 100]]
+
+num_trials = int(10_000)
+with open(BASE_PATH / "param_space" / "invade_param_demes_multi.txt", "w") as f:
+    for graph_folder_name in graph_folder_list:
+        graph_path_list = [
+            f"networks/{graph_folder_name}/" + a for a in os.listdir(BASE_PATH / "networks" / graph_folder_name)
+        ]
+        graph_path_list = sorted(graph_path_list, key=lambda s: tuple(map(int, re.findall(r"\d+", s))))
+                
         for graph_path in graph_path_list:
             f.write(f"{num_objects} {num_sounds} {graph_path} {num_trials}\n")
 
+# %%
 # num_objects=$(sed -n "${line}p" ${param_file} | awk '{print $1}')
 # num_sounds=$(sed -n "${line}p" ${param_file} | awk '{print $2}')
 # graph_path=$(sed -n "${line}p" ${param_file} | awk '{print $4}')
