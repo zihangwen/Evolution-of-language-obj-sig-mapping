21c9c846efc16b1d6d8aeb0ce7b875429ace9630
Zihang Wen <wenzihang0506@gmail.com>
2025-06-24 21:27:25 -0400

    init code_c

---
+++ code_c/language.hpp
@@ -0,0 +1,21 @@
+//  Language model
+//
+//  Created by Zihang Wen on 6/24/2025.
+//  Copyright © 2025 Zihang Wen. All rights reserved.
+//
+
+#pragma once
+
+#include <random>
+#include <ctime>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <vector>
+#include <stdexcept>
+#include <set>
+#include <cmath>
+
+using namespace std;
