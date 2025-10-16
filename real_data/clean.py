64a873df2539fb1dfd6827fe08215371226036fa
Zihang <wenzihang0506@gmail.com>
2025-10-16 09:22:24 -0500

    update clean

--- real_data/clean.py
+++ real_data/clean.py
@@ -9,58 +9,70 @@ BASE_PATH = Path("/home/zihangw/EvoComm/real_data/")
 
 # %% ----- ----- ----- ----- ----- Hunter gather forest ----- ----- ----- ----- ----- %% #
 # Read the file manually
-# df = pd.read_csv(BASE_PATH / "orgdata" / "hunter-gatherer-data" / "aax5913_data_file_s3.txt", sep=" ", names=["source", "target", "weight"])
+df = pd.read_csv(BASE_PATH / "orgdata" / "hunter-gatherer-data" / "aax5913_data_file_s3.txt", sep=" ", names=["source", "target", "weight"])
 
-# # Build the graph
-# G_unweighted = nx.from_pandas_edgelist(df, "source", "target") # , edge_attr="weight")
-# print("Forest: is connected?", nx.is_connected(G_unweighted))
-# print("Forest: num nodes", G_unweighted.number_of_nodes())
-# print("Forest: num edges", G_unweighted.number_of_edges())
-# print("Forest: avg degree", np.mean([d for n, d in G_unweighted.degree()]))
-# print("Forest: avg clustering", nx.average_clustering(G_unweighted))
-# print("Forest: assortativity", nx.degree_assortativity_coefficient(G_unweighted))
-# print("Forest: avg shortest path length", nx.average_shortest_path_length(G_unweighted))
-# print("Forest: diameter", nx.diameter(G_unweighted))
-# print("Forest: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
+# Build the graph
+G_unweighted = nx.from_pandas_edgelist(df, "source", "target") # , edge_attr="weight")
+rename_list = [i for i in range(len(list(G_unweighted.nodes)))]
+mapping = dict(zip(list(G_unweighted.nodes), rename_list))
+G_unweighted = nx.relabel_nodes(G_unweighted, mapping)
 
-# nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / "hg_forest.txt", data=False)
+print("Forest: is connected?", nx.is_connected(G_unweighted))
+print("Forest: num nodes", G_unweighted.number_of_nodes())
+print("Forest: num edges", G_unweighted.number_of_edges())
+print("Forest: avg degree", np.mean([d for n, d in G_unweighted.degree()]))
+print("Forest: avg clustering", nx.average_clustering(G_unweighted))
+print("Forest: assortativity", nx.degree_assortativity_coefficient(G_unweighted))
+print("Forest: avg shortest path length", nx.average_shortest_path_length(G_unweighted))
+print("Forest: diameter", nx.diameter(G_unweighted))
+print("Forest: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
+
+nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / "hg_forest.txt", data=False)
 
 # %% ----- ----- ----- ----- ----- Hunter gather coastal ----- ----- ----- ----- ----- %% #
 # Read the file manually
-# df = pd.read_csv(BASE_PATH / "orgdata" / "hunter-gatherer-data" / "aax5913_data_file_s4.txt", sep=" ", names=["source", "target", "weight"])
+df = pd.read_csv(BASE_PATH / "orgdata" / "hunter-gatherer-data" / "aax5913_data_file_s4.txt", sep=" ", names=["source", "target", "weight"])
 
-# # Build the graph
-# G_unweighted = nx.from_pandas_edgelist(df, "source", "target") # , edge_attr="weight")
-# print("Coastal: is connected?", nx.is_connected(G_unweighted))
-# print("Coastal: num nodes", G_unweighted.number_of_nodes())
-# print("Coastal: num edges", G_unweighted.number_of_edges())
-# print("Coastal: avg degree", np.mean([d for n, d in G_unweighted.degree()]))
-# print("Coastal: avg clustering", nx.average_clustering(G_unweighted))
-# print("Coastal: assortativity", nx.degree_assortativity_coefficient(G_unweighted))
-# print("Coastal: avg shortest path length", nx.average_shortest_path_length(G_unweighted))
-# print("Coastal: diameter", nx.diameter(G_unweighted))
-# print("Coastal: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
+# Build the graph
+G_unweighted = nx.from_pandas_edgelist(df, "source", "target") # , edge_attr="weight")
+rename_list = [i for i in range(len(list(G_unweighted.nodes)))]
+mapping = dict(zip(list(G_unweighted.nodes), rename_list))
+G_unweighted = nx.relabel_nodes(G_unweighted, mapping)
 
-# nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / "hg_coastal.txt", data=False)
+print("Coastal: is connected?", nx.is_connected(G_unweighted))
+print("Coastal: num nodes", G_unweighted.number_of_nodes())
+print("Coastal: num edges", G_unweighted.number_of_edges())
+print("Coastal: avg degree", np.mean([d for n, d in G_unweighted.degree()]))
+print("Coastal: avg clustering", nx.average_clustering(G_unweighted))
+print("Coastal: assortativity", nx.degree_assortativity_coefficient(G_unweighted))
+print("Coastal: avg shortest path length", nx.average_shortest_path_length(G_unweighted))
+print("Coastal: diameter", nx.diameter(G_unweighted))
+print("Coastal: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
+
+nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / "hg_coastal.txt", data=False)
 
 # %% ----- ----- ----- ----- ----- Padgett-Florence-Families ----- ----- ----- ----- ----- %% #
 # # Read the file manually
-# df = pd.read_csv(BASE_PATH / "orgdata" / "Padgett-Florence-Families_Multiplex_Social" / "Dataset" / "Padgett-Florentine-Families_multiplex.edges", sep=" ", names=["layerID", "nodeID1", "nodeID2", "weight"])
+df = pd.read_csv(BASE_PATH / "orgdata" / "Padgett-Florence-Families_Multiplex_Social" / "Dataset" / "Padgett-Florentine-Families_multiplex.edges", sep=" ", names=["layerID", "nodeID1", "nodeID2", "weight"])
 
-# # We only use layerID 1 (marriage) and 2 (business)
-# # df = df[df["layerID"].isin([1, 2])]
+# We only use layerID 1 (marriage) and 2 (business)
+# df = df[df["layerID"].isin([1, 2])]
 
-# # Build the graph
-# G_unweighted = nx.from_pandas_edgelist(df, "nodeID1", "nodeID2") # , edge_attr="weight")
-# print("Padgett-Florence-Families: is connected?", nx.is_connected(G_unweighted))
-# print("Padgett-Florence-Families: num nodes", G_unweighted.number_of_nodes())
-# print("Padgett-Florence-Families: num edges", G_unweighted.number_of_edges())
-# print("Padgett-Florence-Families: avg degree", np.mean([d for n, d in G_unweighted.degree()]))
-# print("Padgett-Florence-Families: avg clustering", nx.average_clustering(G_unweighted))
-# print("Padgett-Florence-Families: assortativity", nx.degree_assortativity_coefficient(G_unweighted))
-# print("Padgett-Florence-Families: avg shortest path length", nx.average_shortest_path_length(G_unweighted))
-# print("Padgett-Florence-Families: diameter", nx.diameter(G_unweighted))
-# print("Padgett-Florence-Families: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
+# Build the graph
+G_unweighted = nx.from_pandas_edgelist(df, "nodeID1", "nodeID2") # , edge_attr="weight")
+rename_list = [i for i in range(len(list(G_unweighted.nodes)))]
+mapping = dict(zip(list(G_unweighted.nodes), rename_list))
+G_unweighted = nx.relabel_nodes(G_unweighted, mapping)
+
+print("Padgett-Florence-Families: is connected?", nx.is_connected(G_unweighted))
+print("Padgett-Florence-Families: num nodes", G_unweighted.number_of_nodes())
+print("Padgett-Florence-Families: num edges", G_unweighted.number_of_edges())
+print("Padgett-Florence-Families: avg degree", np.mean([d for n, d in G_unweighted.degree()]))
+print("Padgett-Florence-Families: avg clustering", nx.average_clustering(G_unweighted))
+print("Padgett-Florence-Families: assortativity", nx.degree_assortativity_coefficient(G_unweighted))
+print("Padgett-Florence-Families: avg shortest path length", nx.average_shortest_path_length(G_unweighted))
+print("Padgett-Florence-Families: diameter", nx.diameter(G_unweighted))
+print("Padgett-Florence-Families: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
 
 # nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / "padgett_florence_families.txt", data=False)
 
@@ -96,7 +108,7 @@ for i_v, village in village_list:
     print(f"Village {village}: diameter", nx.diameter(G))
     print(f"Village {village}: modularity (Louvain)", nx.community.quality.modularity(G, nx.community.louvain_communities(G)))
 
-    nx.write_edgelist(G, BASE_PATH / "cleaned" / f"village_all_{i_v}.txt", data=False)
+    # nx.write_edgelist(G, BASE_PATH / "cleaned" / f"village_all_{i_v}.txt", data=False)
 
 # %% ----- ----- ----- ----- ----- Banerjee et al. Indian villages AND ----- ----- ----- ----- ----- %% #
 # village_list = [
@@ -158,24 +170,28 @@ print("email-Eu: avg shortest path length", nx.average_shortest_path_length(G_un
 print("email-Eu: diameter", nx.diameter(G_unweighted))
 print("email-Eu: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
 
-nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / "email_eu.txt", data=False)
+# nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / "email_eu.txt", data=False)
 
 # %% ----- ----- ----- ----- ----- facebook ----- ----- ----- ----- ----- %% #
 # # Read the file manually
-# df = pd.read_csv(BASE_PATH / "orgdata" / "facebook" / "facebook_combined.txt", sep=" ", names=["nodeID1", "nodeID2"])
+df = pd.read_csv(BASE_PATH / "orgdata" / "facebook" / "facebook_combined.txt", sep=" ", names=["nodeID1", "nodeID2"])
 
-# # Build the graph
-# G_unweighted = nx.from_pandas_edgelist(df, "nodeID1", "nodeID2") # , edge_attr="weight")
+# Build the graph
+G_unweighted = nx.from_pandas_edgelist(df, "nodeID1", "nodeID2") # , edge_attr="weight")
+rename_list = [i for i in range(len(list(G_unweighted.nodes)))]
+mapping = dict(zip(list(G_unweighted.nodes), rename_list))
+G_unweighted = nx.relabel_nodes(G_unweighted, mapping)
 
-# print("facebook: is connected?", nx.is_connected(G_unweighted))
-# print("facebook: num nodes", G_unweighted.number_of_nodes())
-# print("facebook: num edges", G_unweighted.number_of_edges())
-# print("facebook: avg degree", np.mean([d for n, d in G_unweighted.degree()]))
-# print("facebook: avg clustering", nx.average_clustering(G_unweighted))
-# print("facebook: assortativity", nx.degree_assortativity_coefficient(G_unweighted))
-# print("facebook: avg shortest path length", nx.average_shortest_path_length(G_unweighted))
-# print("facebook: diameter", nx.diameter(G_unweighted))
-# print("facebook: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
+
+print("facebook: is connected?", nx.is_connected(G_unweighted))
+print("facebook: num nodes", G_unweighted.number_of_nodes())
+print("facebook: num edges", G_unweighted.number_of_edges())
+print("facebook: avg degree", np.mean([d for n, d in G_unweighted.degree()]))
+print("facebook: avg clustering", nx.average_clustering(G_unweighted))
+print("facebook: assortativity", nx.degree_assortativity_coefficient(G_unweighted))
+print("facebook: avg shortest path length", nx.average_shortest_path_length(G_unweighted))
+print("facebook: diameter", nx.diameter(G_unweighted))
+print("facebook: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
 
 # nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / "facebook.txt", data=False)
 
@@ -209,7 +225,7 @@ for i_fb, fb_separate in fb_separate_list:
     print(f"facebook {fb_separate}: diameter", nx.diameter(G_unweighted))
     print(f"facebook {fb_separate}: modularity (Louvain)", nx.community.quality.modularity(G_unweighted, nx.community.louvain_communities(G_unweighted)))
 
-    nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / f"facebook_{i_fb}.txt", data=False)
+    # nx.write_edgelist(G_unweighted, BASE_PATH / "cleaned" / f"facebook_{i_fb}.txt", data=False)
 
 # %% ----- ----- ----- ----- ----- retweet copen ----- ----- ----- ----- ----- %% #
 # G = nx.read_edgelist(BASE_PATH / "orgdata" / "retweet" / "rt-twitter-copen.mtx")
@@ -235,7 +251,7 @@ print("retweet copen: avg shortest path length", nx.average_shortest_path_length
 print("retweet copen: diameter", nx.diameter(G_max_connected))
 print("retweet copen: modularity (Louvain)", nx.community.quality.modularity(G_max_connected, nx.community.louvain_communities(G_max_connected)))
 
-nx.write_edgelist(G_max_connected, BASE_PATH / "cleaned" / "retweet_copen.txt", data=False)
+# nx.write_edgelist(G_max_connected, BASE_PATH / "cleaned" / "retweet_copen.txt", data=False)
 
 # %%
 
